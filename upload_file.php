f4eca87c4eef33fb56f5fcdbdfc77fcad982ac90
Chao-Chung Kuo <chao-chung.kuo@rwth-aachen.de>
2022-05-20 16:05:21 +0200

    init

---
+++ upload_file.php
@@ -0,0 +1,8 @@
+if(isset($_POST['submit_data']))
+{
+  $uploadfile=$_FILES["upload_file"]["tmp_name"];
+  $folder="data/";
+  move_uploaded_file($_FILES["upload_file"]["tmp_name"], $folder.$_FILES["upload_file"]["name"]);
+  <!-- echo '<img src="'.$folder."".$_FILES["upload_file"]["name"].'">'; -->
+  exit();
+}
